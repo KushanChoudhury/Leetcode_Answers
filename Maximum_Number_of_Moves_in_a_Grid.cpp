3d783140ea9e8b3cda4f16ebaf6a052a8f85ca5a
Kushan Choudhury <kushanchoudhuryoff@gmail.com>
2024-10-30 15:17:58 +0530

    73rd Commit

---
+++ Maximum_Number_of_Moves_in_a_Grid.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int m;
+    int n;
+    vector<int> direction={-1,0,1};
+    int DFS(int row,int col,vector<vector<int>>& grid,vector<vector<int>>& t){
+        if(t[row][col]!=-1)
+            return t[row][col];
+        int moves=0;
+        for(int& dir:direction){
+            int newRow=row+dir;
+            int newCol=col+1;
+            if(newRow>=0&&newRow<m&&newCol>=0&&newCol<n&&grid[newRow][newCol]>grid[row][col])
+                moves=max(moves,1+DFS(newRow,newCol,grid,t));
+        }
+        return t[row][col]=moves;
+    }
+    int maxMoves(vector<vector<int>>& grid) {
+        m=grid.size();
+        n=grid[0].size();
+        int result=0;
+        vector<vector<int>> t(m,vector<int>(n,-1));
+        for(int i=0;i<m;i++){
+            result=max(result,DFS(i,0,grid,t));
+        }
+        return result;
+
+    }
+};
